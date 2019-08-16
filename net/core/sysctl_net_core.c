12a7cbd2904bb4518082806ccae83f63ccda2e8f
Daniel Borkmann <daniel@iogearbox.net>
2019-08-16 23:59:56 +0100

    bpf: restrict access to core bpf sysctls
    commit 2e4a30983b0f9b19b59e38bbf7427d7fdd480d98 upstream.
    
    Given BPF reaches far beyond just networking these days, it was
    never intended to allow setting and in some cases reading those
    knobs out of a user namespace root running without CAP_SYS_ADMIN,
    thus tighten such access.
    
    Also the bpf_jit_enable = 2 debugging mode should only be allowed
    if kptr_restrict is not set since it otherwise can leak addresses
    to the kernel log. Dump a note to the kernel log that this is for
    debugging JITs only when enabled.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    [bwh: Backported to 4.9:
     - We don't have bpf_dump_raw_ok(), so drop the condition based on it. This
       condition only made it a bit harder for a privileged user to do something
       silly.
     - Drop change to bpf_jit_kallsyms]
    Signed-off-by: Ben Hutchings <ben.hutchings@codethink.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -266,18 +266,7 @@ proc_dointvec_minmax_bpf_restricted(struct ctl_table *table, int write,
 
 	return proc_dointvec_minmax(table, write, buffer, lenp, ppos);
 }
-# endif /* CONFIG_HAVE_EBPF_JIT */
-
-static int
-proc_dolongvec_minmax_bpf_restricted(struct ctl_table *table, int write,
-				     void __user *buffer, size_t *lenp,
-				     loff_t *ppos)
-{
-	if (!capable(CAP_SYS_ADMIN))
-		return -EPERM;
-
-	return proc_doulongvec_minmax(table, write, buffer, lenp, ppos);
-}
+# endif
 #endif
 
 static struct ctl_table net_core_table[] = {
@@ -341,7 +330,7 @@ static struct ctl_table net_core_table[] = {
 		.data		= &bpf_jit_enable,
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
-		.proc_handler	= proc_dointvec_minmax,
+		.proc_handler	= proc_dointvec_minmax_bpf_enable,
 # ifdef CONFIG_BPF_JIT_ALWAYS_ON
 		.extra1		= &one,
 		.extra2		= &one,
@@ -356,7 +345,7 @@ static struct ctl_table net_core_table[] = {
 		.data		= &bpf_jit_harden,
 		.maxlen		= sizeof(int),
 		.mode		= 0600,
-		.proc_handler	= proc_dointvec_minmax,
+		.proc_handler	= proc_dointvec_minmax_bpf_restricted,
 		.extra1		= &zero,
 		.extra2		= &two,
 	},
